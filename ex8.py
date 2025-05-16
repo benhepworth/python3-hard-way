8fd819db32c30f2bfb31aa43d8ca0ff973b5645b
Ben Hepworth <b.hepworth@cablelabs.com>
2025-05-16 07:52:57 -0600

    Add exercise 8

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Oh the misery",
+    "everyone wants to be my enemy",
+    "spare the sympathy",
+    "everybody wants to be my enemy"
+))
