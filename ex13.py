103d1211d847e9d6c23a587b71e2b890a6cac297
Ben Hepworth <b.hepworth@cablelabs.com>
2025-05-20 16:24:32 -0600

    Add exercise 13

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+#script = argv[0]
+#first = argv[1]
+#second = argv[2]
+#third = argv[3]
+
+print("The script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("The third variable is:", third)
